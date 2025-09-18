20bc31ec09591db11f5c4b6c7f45f1b8853f83c6
shi-feng-wu <shifengwu241@gmail.com>
2025-09-18 13:57:02 -0400

    articles

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,10 @@ const allArticles = [
     id: 1,
     title:
       "Religion, Ethics, and Medicine at End of Life: When It Is Acceptable for Physicians to Refuse Care?",
-    author: "Luo, G. (Adam)",
+    author: "Guanxi Adam Luo",
     school: "",
     image: "/images/optimized/ethics-1600.webp",
-    abstract: `In this article, the author considers physicians’ right to refuse to participate in or offer
+    abstract: `In this article, the author considers physicians' right to refuse to participate in or offer
 end-of-life care that includes medically assisted death. The author argues that physicians, like
 patients, are guided by religious and more beliefs, and they do have a right to refuse medically
 assisted death services if they violate their religious beliefs or moral convictions. To make this
@@ -23,14 +23,14 @@ presented to further illustrate how religious beliefs affect the care that a per
 end of life. After reviewing possible reasons that a physician may object on religious grounds,
 the author then provides recommendations that honor both the free will of the patient and the
 physician.`,
-    publishDate: "2025-09-16",
+    publishDate: "2025-06-10",
     category: "Ethics & Society",
     link: "/articles/adam-religion-and-medicine-article-pdf.pdf",
   },
   {
     id: 2,
     title: "Leadership Education for International Students",
-    author: "Ma, C. F.",
+    author: "Francella Ochillo, Clara Ma",
     school: "",
     image: "/images/optimized/neuroleadership-1600.webp",
     abstract: `International students in U.S. higher education face increasing uncertainty, not only due to
@@ -41,14 +41,15 @@ context, and global ethics scholarship, the piece calls for a reimagining of lea
 development as a core element of higher education and an essential navigational tool for future
 leaders. A global moral compass, as defined by Thompson (2010), offers an essential
 framework for international students navigating complex and inequitable systems.`,
-    publishDate: "2025-09-16",
+    publishDate: "2025-07-20",
     category: "Leadership & Education",
     link: "/articles/leadership-education-for-international-students.pdf",
   },
   {
     id: 3,
-    title: "Neuroleadership and Neuroeducation",
-    author: "Hardiman, M. & Ma, C. F.",
+    title:
+      "From Neurons to Leaders: A Brain-Targeted Framework for Leadership Education",
+    author: "Mariale Hardiman",
     school: "Johns Hopkins University",
     image: "/images/optimized/leadership-800.webp",
     abstract: `The rapidly evolving fields of neuroleadership and neuroeducation hold immense potential for
@@ -70,7 +71,7 @@ Ultimately, this article seeks to arm leaders with a brain-targeted approach to 
 learning, helping them to create impactful experiences that empower individuals to thrive and
 realize their full potential.
 `,
-    publishDate: "2025-09-16",
+    publishDate: "2025-08-20",
     category: "Neuroscience & Cognitive Science",
     link: "/articles/neuroleadership-and-neuroeducation.pdf",
   },
@@ -78,15 +79,15 @@ realize their full potential.
     id: 4,
     title:
       "Meet Your Therapist: Exploring the Promise and Drawbacks of AI for Treating Digital Addictive Behavior among Adolescents",
-    author: "Gao, R.",
+    author: "Ray Gao",
     school: "",
     image: "/images/optimized/therapy-800.webp",
     abstract: `Within the public health domain, one of greatest concerns is the rise of addictive behavior among
 adolescents and young adults. Questions have been raised as to how excessive video gaming,
 social media overuse of misuse, and online gambling, for instance, present deleterious effects to
-this population’s psychological well-being as well as their overall development. In light of these
+this population's psychological well-being as well as their overall development. In light of these
 concerns, public health officials, policymakers, and mental health professionals have set out to
-explore effective interventions designed to address this issue and improve this population’s
+explore effective interventions designed to address this issue and improve this population's
 quality of life and health outcomes. Due to its widespread accessibility, low-cost, relative
 anonymity, and room for personalization and user engagement, artificial intelligence (AI) is one
 such intervention currently being explored. This paper therefore joins the ongoing conversation
@@ -95,9 +96,63 @@ to consider areas of concern in its application. Reviewing and synthesizing the 
 on this topic thus allows this paper to offer the view that although AI should not be used in lieu
 of human mental health providers, it can serve as an auxiliary resource that complements
 existing approaches to provide more comprehensive care to those in need.`,
-    publishDate: "2025-09-16",
+    publishDate: "2025-08-18",
     category: "Technology & Innovation",
     link: "/articles/ray-gao-palo.pdf",
+  },
+  {
+    id: 5,
+    title:
+      "Negotiating girl power in contemporary China: Feminist possibilities and contradictions.",
+    author: "Ashley Yu, Iris Zhu",
+    image: "/images/optimized/feminism-1600.webp",
+    abstract: `In recent years, the term girl power (女孩力量) has become increasingly prominent in Chinese  
+discourse, yet its meanings remain insufficiently theorized within the country's unique  
+sociocultural landscape. This article offers a qualitative literature synthesis to explore how girl  
+power is being conceptualized, articulated, and negotiated in contemporary China. Drawing on  
+29 qualitative studies published between 2005 and 2024, we use a meta-ethnographic approach  
+to identify five overarching themes: Gender Norms & Power Structures, Identity & Self 
+Determination, Media Female Imagery, Body Aesthetic Politics, and Social Consumption  
+Narratives. Our analysis reveals that girl power in China is neither a straightforward import of  
+Western feminist ideals nor a passing pop-cultural trend. Rather, it reflects a dynamic interplay  
+between tradition and modernity, digital expression and state control, empowerment and  
+commodification. While some narratives challenge patriarchal norms and enable self-expression,  
+others risk reinscribing inequality through market-driven “pseudo-feminist” frameworks. We  
+argue that girl power in China functions as both a site of agency and a mechanism of social  
+regulation, complicating linear understandings of feminist progress. This study contributes to  
+feminist theory by situating girl power within China’s hybrid sociopolitical landscape and offers  
+a conceptual baseline for future empirical and theoretical inquiry.`,
+    publishDate: "2025-05-18",
+    category: "Gender Studies & Society",
+    link: "/articles/Girl power.pdf",
+  },
+  {
+    id: 6,
+    title:
+      "Making Our Seas Sustainable: Examining Iceland’s Maritime Sector through a Commercial, Legal, and Ethical Lens",
+    author: "Jiahong Julia Fu, Siyi Lisa Feng",
+    image: "/images/optimized/iceland-1600.webp",
+    abstract: `Through the introduction of a tri-part framework focused on commercial, legal, and ethical 
+considerations, this article examines the case of Iceland’s fishing industry to explore key 
+concepts of sustainability. The article focuses on the fishing sector, specifically, as it is poised to 
+grow exponentially in the coming years, as the world looks to more eco-friendly alternatives for 
+land-based meat (e.g., beef, pork). Iceland was selected as the ideal case study for exploring 
+these concepts because not only is the fishing sector well-established within the nation, but 
+because the country’s industry has incorporated environmental, social, and governance (ESG) 
+standards into its operations. Iceland’s fisheries demonstrate ecological stewardship through the 
+use of a monitoring system that collects data on the total number and type of species caught, 
+utilizing all parts of the fish and minimizing waste, and employing responsible harvesting 
+methods. They exemplify social responsibility by adopting fair labor laws at the federal level that 
+protect temporary and foreign workers and engaging in responsible marketing and ecolabeling. 
+While Icelandic fisheries are presented as pioneers in sustainable business, this discussion also 
+attends to areas for improvement, including issues pertaining to equal access to fishing rights 
+among smaller or newer companies and concerns for animal welfare in farm-raised fishing and 
+open-water catches. However, both the areas where Iceland’s fishing industry excels and falls 
+short represent key considerations that can ideally be applied to a variety of other cultural contexts and economic sectors so that true sustainability is no longer seen as bound by region or 
+industry.`,
+    publishDate: "2025-08-17",
+    category: "Environmental Science & Sustainability",
+    link: "/articles/Lisa and Julie _ch.pdf",
   },
 ];
 
