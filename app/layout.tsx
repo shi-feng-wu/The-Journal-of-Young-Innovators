b2d7d3b0ecb32cca66b596ffc5261b2d77f5be6c
shi-feng-wu <103332578+shi-feng-wu@users.noreply.github.com>
2025-12-18 15:07:45 -0500

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { MuseoModerno } from "next/font/google";
-import { ReactLenis, useLenis } from "lenis/react";
 
 import "./globals.css";
 import { Providers } from "./providers.tsx";
